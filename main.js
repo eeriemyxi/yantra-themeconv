528e27e9bdbcf6e75a3ca2d662f9d4ee35c37f6d
eeriemyxi <myxi@envs.net>
2025-12-17 10:31:19 +0530

    feat: big bang

---
+++ main.js
@@ -0,0 +1,39 @@
+const SUBSTITUTES = {
+    "Theme Name": "name",
+    "Background Color": "bgColor",
+    "Command Color": "cmdColor",
+    "Suggestions Color": "suggestionsColor",
+    "Suggestions Bar Color": "suggestionsBgColor",
+    "Input Color": "inputAndBtnsColor",
+    "Normal Text & Arrow Color": "resultColor",
+    "Error Text Color": "errorColor",
+    "Positive Text Color": "successColor",
+    "Warning Text Color": "warnColor"
+}
+
+function oldToNewConfig(text) {
+    const newConfig = {}
+
+    for (const line of text.split("\n")) {
+        const data = line.split(":** ")
+        const key = data[0].slice(2)
+        const value = data[1]
+        if (key in SUBSTITUTES) newConfig[SUBSTITUTES[key]] = value
+    }
+
+    return newConfig
+}
+
+async function convertButton() {
+    const oldconftag = document.getElementById("old");
+    const convbtntag = document.getElementById("convbtn");
+
+    const newconfig = oldToNewConfig(oldconftag.value)
+    navigator.clipboard.writeText(JSON.stringify(newconfig))
+
+    const _oldtxt = convbtntag.textContent
+    convbtntag.textContent = "Result copied to clipboard!"
+    oldconftag.value = ""
+    await new Promise(r => setTimeout(r, 2000));
+    convbtntag.textContent = _oldtxt
+}
